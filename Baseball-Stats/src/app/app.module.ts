0c462b03b7723480e023e292de66f6915f897c42
tyjdor <tyjdor96@gmail.com>
2021-03-26 19:34:13 -0400

    init project

---
+++ Baseball-Stats/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PlayersComponent } from './players/players.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PlayersComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
